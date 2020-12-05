2a876662f3a78552dc9b12e21218b72512f495cb
nurzainpradana <pradananurzain@gmail.com>
2020-12-05 15:21:16 +0700

    inital commit

---
+++ app/src/main/java/com/rizen/aplikasirequestmaintenance/view/splashscreen/MainActivity.java
@@ -0,0 +1,16 @@
+package com.rizen.aplikasirequestmaintenance.view.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rizen.aplikasirequestmaintenance.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
